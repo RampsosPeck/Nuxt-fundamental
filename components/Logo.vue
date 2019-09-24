78b634c3aeae0549421e1de02bb958f5f46226ef
RampsosPeck <peralta.jorge.uatf@gmail.com>
2019-09-24 02:55:28 -0400

    Finish cource complet

---
+++ components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <img src="https://vueschool.io/img/favicons/apple-touch-icon.png" alt="the logo">
+
+</template>
+<script>
+	export default {
+  components: {
+
+  },
+  computed: {
+    posts () {
+      return this.$store.state.posts.all
+    }
+  }
+}
+</script>
